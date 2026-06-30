ff781f6c9d8363fd2f784165b77bca252338d777
jrkkhan <jrkkhan93@gmail.com>
2026-06-30 13:59:02 +0530

    Optimize Google Tag Manager loading for PageSpeed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
-import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -84,10 +84,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${roboto.variable} ${nataSans.variable} h-full antialiased`} suppressHydrationWarning>
-      <GoogleTagManager gtmId="GTM-5K96GNBB" />
       <body className="min-h-full flex flex-col font-sans">
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5K96GNBB"
+          height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
         {children}
         <FloatingContact />
+        <Script id="google-tag-manager" strategy="lazyOnload">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5K96GNBB');
+          `}
+        </Script>
       </body>
     </html>
   );
